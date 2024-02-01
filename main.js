c1e8f51f146ca077926abafc1db2d1d76045c5c0
Joaco Virgili <jvirgili@mail.austral.edu.ar>
2024-02-01 18:51:59 -0300

    Cambios

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// document.body.addEventListener('mousemove',
-// (event)=>{
-//     const {clientX, clientY} = event;
-
-//     gsap.set('.cursor', {
-//         x: clientX,
-//         y: clientY
-//     })
-
-//     gsap.to('.shape', {
-//         x: clientX,
-//         y: clientY,
-//         stagger: -0.1
-//     })
-// });
 
 
 document.getElementById('botonFlotante').addEventListener('click', function() {
